ecee44160f2047129561408406fd5fd92e8651b3
Stephen Little <47438829+Stevish1968@users.noreply.github.com>
2022-02-18 17:49:11 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cluster = require('cluster');
+const {
+    cpus
+} = require('os');
+const process = require('process');
+
+cluster.schedulingPolicy = cluster.SCHED_RR; // this has been added 
+
+const app = express();
+
+function delay(duration) {
+    const startTime = Date.now();
+    while (Date.now() - startTime < duration) {
+        //event loop is blocked. ..
+    }
+}
+
+app.get('/', (req, res) => {
+    res.send(`Performance example ${process.pid}`);
+});
+
+app.get('/timer', (req, res) => {
+    //delay the response
+    delay(9000);
+    res.send(`Ding ding ding ${process.pid}`);
+});
+
+console.log('Running server.js...');
+console.log(`Worker process started ${process.pid}`);
+app.listen(3000);
